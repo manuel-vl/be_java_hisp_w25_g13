c38c209ea4650d1d7af999c2fd86b79e19b40688
manuel-vl <manuel.valencia@mercadolibre.com.co>
2024-02-19 13:40:52 -0500

    [FEATURE] Add Product Service

--- src/main/java/com/be_java_hisp_w25_g13/be_java_hisp_w25_g13/repository/ProductRepositoryImpl.java
+++ src/main/java/com/be_java_hisp_w25_g13/be_java_hisp_w25_g13/repository/ProductRepositoryImpl.java
@@ -1,10 +1,12 @@
 package com.be_java_hisp_w25_g13.be_java_hisp_w25_g13.repository;
 
 import com.be_java_hisp_w25_g13.be_java_hisp_w25_g13.entity.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class ProductRepositoryImpl implements IProductRepository{
     List<Product> products=new ArrayList<>();
 
--- src/main/java/com/be_java_hisp_w25_g13/be_java_hisp_w25_g13/service/IProductService.java
+++ src/main/java/com/be_java_hisp_w25_g13/be_java_hisp_w25_g13/service/IProductService.java
@@ -1,4 +1,10 @@
 package com.be_java_hisp_w25_g13.be_java_hisp_w25_g13.service;
 
+import com.be_java_hisp_w25_g13.be_java_hisp_w25_g13.dto.ProductDTO;
+
+import java.util.List;
+
 public interface IProductService {
+    ProductDTO addProduct(ProductDTO productDTO);
+    List<ProductDTO> getProducts();
 }
