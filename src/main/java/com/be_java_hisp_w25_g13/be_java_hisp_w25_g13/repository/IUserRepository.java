795c9dfbfb91b1bfbceac663ea04a12871e8ff15
manuelrestrep021 <juanmanuel.restrepogalarcio@mercadolibre.com.co>
2024-02-19 08:31:18 -0500

    cambios generales para la branch main

--- src/main/java/com/be_java_hisp_w25_g13/be_java_hisp_w25_g13/repository/IUserRepository.java
+++ src/main/java/com/be_java_hisp_w25_g13/be_java_hisp_w25_g13/repository/IUserRepository.java
@@ -4,11 +4,13 @@ import com.be_java_hisp_w25_g13.be_java_hisp_w25_g13.entity.Seller;
 import com.be_java_hisp_w25_g13.be_java_hisp_w25_g13.entity.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IUserRepository {
     User addUser(User user);
     List<User> getAll();
-    User getUserById(Integer userId);
-    Seller getSellerById(Integer userId);
+    Optional<User> getUserById(Integer userId);
+    Optional<Seller> getSellerById(Integer userId);
+
 
 }
