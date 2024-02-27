c12812cd68b41fa605ed3701fc9cbcf2ebbe9d27
ManuelDevWeb <manueldeveloper17@gmail.com>
2024-02-27 13:24:43 -0500

    [FEATURE] Add Utilities file and functions structure

--- src/test/java/com/be_java_hisp_w25_g13/be_java_hisp_w25_g13/service/PostServiceImplTest.java
+++ src/test/java/com/be_java_hisp_w25_g13/be_java_hisp_w25_g13/service/PostServiceImplTest.java
@@ -1,7 +1,11 @@
 package com.be_java_hisp_w25_g13.be_java_hisp_w25_g13.service;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class PostServiceImplTest {
 
 }
--- src/test/java/com/be_java_hisp_w25_g13/be_java_hisp_w25_g13/utils/Utilities.java
+++ src/test/java/com/be_java_hisp_w25_g13/be_java_hisp_w25_g13/utils/Utilities.java
@@ -1,4 +1,23 @@
 package com.be_java_hisp_w25_g13.be_java_hisp_w25_g13.utils;
 
+import com.be_java_hisp_w25_g13.be_java_hisp_w25_g13.dto.UserDTO;
+import com.be_java_hisp_w25_g13.be_java_hisp_w25_g13.entity.Seller;
+import com.be_java_hisp_w25_g13.be_java_hisp_w25_g13.entity.User;
+
+import java.util.List;
+
 public class Utilities {
+    public static UserDTO generateUserDTO(){
+        return null;
+    }
+    public static List<UserDTO> generateUsersDTO(){
+        return null;
+    }
+    public static Seller generateSeller(){
+        return null;
+    }
+    public static User generateUser(){
+        return null;
+    }
+
 }
