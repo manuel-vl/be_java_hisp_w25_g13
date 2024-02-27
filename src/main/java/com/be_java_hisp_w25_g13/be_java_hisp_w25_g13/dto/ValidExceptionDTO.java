ff1cf7f7ea6038c19b334dbc4165c1a9d0e455e5
manuelrestrepo21 <juanmanuel.restrepogalarcio@mercadolibre.com.co>
2024-02-27 15:17:10 -0500

    exception valid

--- src/main/java/com/be_java_hisp_w25_g13/be_java_hisp_w25_g13/dto/ValidExceptionDTO.java
+++ src/main/java/com/be_java_hisp_w25_g13/be_java_hisp_w25_g13/dto/ValidExceptionDTO.java
@@ -2,5 +2,5 @@ package com.be_java_hisp_w25_g13.be_java_hisp_w25_g13.dto;
 
 import java.util.List;
 
-public record ExceptionDTO(String message) {
+public record ValidExceptionDTO(List<String> messages) {
 }
--- src/main/java/com/be_java_hisp_w25_g13/be_java_hisp_w25_g13/exception/ExceptionHandlers.java
+++ src/main/java/com/be_java_hisp_w25_g13/be_java_hisp_w25_g13/exception/ExceptionHandlers.java
@@ -1,13 +1,19 @@
 package com.be_java_hisp_w25_g13.be_java_hisp_w25_g13.exception;
 
 import com.be_java_hisp_w25_g13.be_java_hisp_w25_g13.dto.ExceptionDTO;
+import com.be_java_hisp_w25_g13.be_java_hisp_w25_g13.dto.ValidExceptionDTO;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class ExceptionHandlers {
@@ -25,8 +31,17 @@ public class ExceptionHandlers {
         return new ResponseEntity<>(new ExceptionDTO(e.getMessage()), HttpStatus.CONFLICT);
     }
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ExceptionDTO> methodArgumentNotValid(MethodArgumentNotValidException e) {
-        return new ResponseEntity<>(new ExceptionDTO(e.getMessage()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ValidExceptionDTO> methodArgumentNotValid(MethodArgumentNotValidException e) {
+        List<String> errorMessages = new ArrayList<>();
+        for(ObjectError error: e.getBindingResult().getAllErrors()){
+            if(error instanceof FieldError){
+                FieldError fieldError = (FieldError) error;
+                errorMessages.add(fieldError.getField() + ": " + fieldError.getDefaultMessage());
+            } else {
+                errorMessages.add(error.getDefaultMessage());
+            }
+        }
+        return new ResponseEntity<>(new ValidExceptionDTO(errorMessages), HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ExceptionDTO> methodArgumentTypeMismatch(MethodArgumentTypeMismatchException e) {
