d710e7eedd587c4a54decf3265d1ea6063aacc19
Juan Felipe Peña <juanfelipe.pena@mercadolibre.com.co>
2024-02-29 08:28:06 -0500

    [feat] add DTO validations

--- src/main/java/com/be_java_hisp_w25_g13/be_java_hisp_w25_g13/dto/ProductDTO.java
+++ src/main/java/com/be_java_hisp_w25_g13/be_java_hisp_w25_g13/dto/ProductDTO.java
@@ -14,20 +14,25 @@ public class ProductDTO {
 
     @NotBlank(message = "El campo no puede estar vacío")
     @Pattern(regexp = "[a-zA-Z0-9 ]+", message = "El campo no puede poseer caracteres especiales")
+    @Size(max = 40, message = "La longitud no puede superar los 40 caracteres")
     private String productName;
 
     @NotEmpty(message = "El campo no puede estar vacío")
     @Pattern(regexp = "[a-zA-Z0-9 ]+", message = "El campo no puede poseer caracteres especiales")
+    @Size(max = 15, message = "La longitud no puede superar los 15 caracteres")
     private String type;
 
     @NotEmpty(message = "El campo no puede estar vacío")
     @Pattern(regexp = "[a-zA-Z0-9 ]+", message = "El campo no puede poseer caracteres especiales")
+    @Size(max = 25, message = "La longitud no puede superar los 25 caracteres")
     private String brand;
 
     @NotEmpty(message = "El campo no puede estar vacío")
     @Pattern(regexp = "[a-zA-Z0-9 ]+", message = "El campo no puede poseer caracteres especiales")
+    @Size(max = 15, message = "La longitud no puede superar los 15 caracteres")
     private String color;
 
     @Pattern(regexp = "[a-zA-Z0-9 ]+", message = "El campo no puede poseer caracteres especiales")
+    @Size(max = 15, message = "La longitud no puede superar los 80 caracteres")
     private String notes;
 }
